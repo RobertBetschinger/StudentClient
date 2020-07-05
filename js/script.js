5a2890c0d3f0c50169f8d33029e9c980f46e5570
Robert Betschinger <robert-betschinger@outlook.de>
2020-07-05 20:01:01 +0200

    Added Peer Discussion Time Function

--- js/script.js
+++ js/script.js
@@ -42,6 +42,56 @@ socket.on("Group-Changed", (users) => {
   fillInDataInDropdown(users);
 });
 
+socket.on("DiscussionTime", (value) =>{
+  init(value)
+})
+
+function init(minutes) {
+	console.log('init');
+	var time_in_minutes = minutes;
+  start_countdown('div_clock', time_in_minutes);
+
+}
+
+function start_countdown(clockid, time_in_minutes) {
+	console.log("start_countdown");
+	//start the countdown
+	var current_time = Date.parse(new Date());
+	var deadline = new Date(current_time + time_in_minutes * 60 * 1000);
+	run_clock(clockid, deadline);
+}
+
+
+var timeinterval
+function run_clock(id, endtime) {
+	console.log("run_clock");
+  var clock = document.getElementById(id);
+  
+  
+  
+  function update_clock() {
+		var t = time_remaining(endtime);
+		clock.innerHTML = ((t.minutes).toString()).padStart(2, '0') + ':' + ((t.seconds).toString()).padStart(2, '0') + ' left';
+		if (t.total <= 0) {
+			alert("Zeit abgelaufen")
+      clearInterval(timeinterval);		
+		}
+  }
+  clearInterval(timeinterval)
+	update_clock(); // run function once at first to avoid delay
+ timeinterval = setInterval(update_clock, 1000);
+}
+
+function time_remaining(endtime) {
+	console.log("time_remaining")
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return { 'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds };
+}
+
 const selectNames = document.getElementById("namesSelect");
 function fillInDataInDropdown(users) {
   console.log("Update Group List");
@@ -65,14 +115,29 @@ function loadSelectedChatRoom() {
   return value;
 }
 
-socket.on("showStatistics", (question, data) => {
+socket.on("showStatistics", (data) => {
   console.log("Statistics arrived");
-  adddata(data);
+  console.log(data)
+  var AnswerCount ={
+    ant1: data.count1,
+    ant2: data.count2,
+    ant3: data.count3,
+    ant4: data.count4,
+  }
+
+  var question ={
+    antwort1: data.ant1,
+    antwort2: data.ant2,
+    antwort3: data.ant3,
+    antwort4: data.ant4,
+  }
+  adddata(AnswerCount);
   changeLabels(question);
   showStatistics(true);
 });
 
 socket.on("newPhase", (message) => {
+  endTimer();
   console.log("New Phase");
   if (message == "Lecteuring") {
     Fragenblock.style.visibility = "hidden";
@@ -80,6 +145,12 @@ socket.on("newPhase", (message) => {
   changePhase(message);
 });
 
+
+function endTimer(){
+  var clock = document.getElementById('div_clock');
+  clearInterval(timeinterval)
+  clock.innerHTML =""
+}
 
 socket.on("private-message", (privMessageObj) => {
   appendMessagePrivateOthers(privMessageObj.name, privMessageObj.message);
